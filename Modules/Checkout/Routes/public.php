9a89fd44dfc105571ff5b3073107e30788c04f49
Nytro Dev <nytrotechdev>
2021-12-28 19:13:53 +0500

    update issue

--- Modules/Checkout/Routes/public.php
+++ Modules/Checkout/Routes/public.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::middleware('auth')->group(function () {
 
 Route::get('checkout', 'CheckoutController@create')->name('checkout.create');
 Route::post('checkout', 'CheckoutController@store')->name('checkout.store');
+});
 
 Route::get('checkout/{orderId}/complete', 'CheckoutCompleteController@store')->name('checkout.complete.store');
 Route::get('checkout/complete', 'CheckoutCompleteController@show')->name('checkout.complete.show');

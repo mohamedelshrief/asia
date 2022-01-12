89c96f01c16bfef26655a4a768463639af3bca64
Nytro Dev <nytrotechdev>
2022-01-12 17:01:40 +0500

    shipping

--- Modules/Setting/Resources/views/admin/settings/tabs/emiratespost.blade.php
+++ Modules/Setting/Resources/views/admin/settings/tabs/emiratespost.blade.php
@@ -6,23 +6,6 @@ use \Modules\Setting\Entities\Country;
         {{ Form::checkbox('emirates_post_enabled', trans('setting::attributes.emirates_post_enabled'), trans('setting::settings.form.enable_emirates_post'), $errors, $settings) }}
         {{ Form::text('emirates_post_label', trans('setting::attributes.translatable.emirates_post_label'), $errors, $settings, ['required' => true]) }}
 
-
-
-        <div class="form-group">
-            <label class="col-md-3 control-label text-left">{{trans('setting::attributes.translatable.emirates_post_country')}}<span class="m-l-5 text-red">*</span></label>
-            <div class="col-md-9">
-                <select class="form-contol" name="emirates_country" id="emirates_country" required="true" >
-                    <option >{{trans('setting::attributes.translatable.emirates_post_country')}}</option>
-                    @foreach (Country::get() as $item)
-                    @if ($settings["emirates_country"]==$item->country_id)
-                        <option value="{{$item->country_id}}" selected >{{$item->country_name}}</option>
-                    @else
-                        <option value="{{$item->country_id}}">{{$item->country_name}}</option>
-                    @endif
-                    @endforeach
-                </select>
-            </div>
-        </div>
     </div>
 </div>
 

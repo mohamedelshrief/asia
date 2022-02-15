918413d62d79a952e99aa1ef92f2ca40f21b98b9
Nytro Dev <nytrotechdev>
2022-02-15 13:33:58 +0500

    apple login issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -40,7 +40,7 @@ $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     FleetCart\Exceptions\Handler::class
 );
-
+$app->register(FleetCart\Providers\EventServiceProvider::class);
 /*
 |--------------------------------------------------------------------------
 | Return The Application

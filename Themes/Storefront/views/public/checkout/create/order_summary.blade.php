ccaad74cb4fc2ae112921b5eef289848a7d4a9da
Syed M Sohaib <syedmsohaib68@gmail.com>
2022-10-29 12:48:40 +0500

    fxes

--- Themes/Storefront/views/public/checkout/create/order_summary.blade.php
+++ Themes/Storefront/views/public/checkout/create/order_summary.blade.php
@@ -62,7 +62,7 @@ use Modules\Cart\Facades\Cart;
             </ul>
             <div class="shipping-methods" v-if="hasShippingMethod" v-cloak>
                 <h6>{{ trans('storefront::cart.shipping_method') }}</h6>
-                @if (isset(Cart::availableShippingMethods()->flat_rate->cost->amount))
+                @if (isset(son_decode(Cart::availableShippingMethods())->flat_rate->cost->amount))
                 <input type="hidden" id="shipping_cost_amount" name="shipping_cost" value="{{json_decode(Cart::availableShippingMethods())->flat_rate->cost->amount}}" />
                 @endif
                 @if(isset(Cart::total()->amount()))

7fcb881e0655080cc140aeec037095a58c3b9056
Syed M Sohaib <syedmsohaib68@gmail.com>
2022-12-05 12:53:22 +0500

    changes

--- Modules/Cart/Http/Controllers/CartController.php
+++ Modules/Cart/Http/Controllers/CartController.php
@@ -85,7 +85,7 @@ class CartController
                 'RateCalculation' => [
                     'RateList' => [
                         [
-                            'TotalPriceAED' => Cart::shippingMethod()->cost()->amount(),
+                            'TotalPriceAED' => 15,
                         ]
                     ]
                 ]
